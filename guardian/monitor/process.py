6fc3030b585413cdbb7a20baf9fc9e4064fdc33c
commonrover64 <siddhartharampalsingh@gmail.com>
2026-08-05 00:53:16 +0530

    renamed Process class fields for clarity and added process_parent_name

--- guardian/monitor/process.py
+++ guardian/monitor/process.py
@@ -1,5 +1,6 @@
 class Process:
-    def __init__(self, pid, ppid, name):
-        self.pid = pid  
-        self.ppid = ppid
-        self.name = name
+    def __init__(self, id, name, parent_id, parent_name):
+        self.id = id
+        self.name = name    
+        self.parent_id = parent_id
+        self.parent_name = parent_name
