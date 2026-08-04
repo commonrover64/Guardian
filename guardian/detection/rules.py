b2f969f70cd8cbb01d21ea6f7f73d54c95308a28
commonrover64 <siddhartharampalsingh@gmail.com>
2026-08-05 00:57:35 +0530

    updated process.pid to process.id to match updated Process class metadata and added 2nd rule: suspicious parent-child process rule

--- guardian/detection/rules.py
+++ guardian/detection/rules.py
@@ -1,9 +1,10 @@
 import os
 
+# 1st rule
 def get_real_path(process):
     process_real_path = "not found yet"
     try:
-        process_real_path = os.readlink(f"/proc/{process.pid}/exe")
+        process_real_path = os.readlink(f"/proc/{process.id}/exe")
     except FileNotFoundError:
         print("error finding real path")
 
@@ -11,3 +12,25 @@ def get_real_path(process):
 
 def is_from_temp(path):
     return path == "/tmp" or path.startswith("/tmp/")
+
+# 2nd rule
+# these parent/child pairings are just example and does not always mean the activity is malicious
+suspicious_parent_child_relation = {
+    "nginx": {"bash", "sh", "zsh", "dash"},
+    "apache2": {"bash", "sh"},
+    "httpd": {"bash", "sh"},
+    "php-fpm": {"bash", "sh"},
+    "java": {"bash", "sh"},
+    "firefox": {"bash", "sh"},
+    "chrome": {"bash", "sh"},
+    "libreoffice": {"bash", "sh"},
+}
+
+def suspicious_relation(process):
+    parent = process.parent_name
+    child = process.name
+
+    if parent is None or child is None:
+        return False
+
+    return child in suspicious_parent_child_relation.get(parent, set()) # returns all the childs for this parent in set and then checks if the current child is one of them
