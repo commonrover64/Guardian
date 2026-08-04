d46ac28c6dabd6e6e774ba126f30f46c99d88ceb
commonrover64 <siddhartharampalsingh@gmail.com>
2026-08-05 01:08:13 +0530

    updated alert with new process field reference

--- guardian/ouput/formatter.py
+++ guardian/ouput/formatter.py
@@ -1,6 +1,6 @@
 def print_alert(process, reason):
     print(f"\n! SUSPICIOUS PROCESS DETECTED !")
-    print(f"    Process : {process.name} (PID {process.pid})")
-    print(f"    Parent  : PID {process.ppid}")
+    print(f"    Process : {process.name} (PID {process.id})")
+    print(f"    Parent  : PID {process.parent_id}")
     print(f"    Reason  : {reason}")
     print()
